f34c6aaf6de4fbaebbd91788474945622535789c
nguyenducanh0157 <nguyenducanh0157>
2024-11-17 00:24:58 +0700

    update author

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,28 +1,33 @@
-import { auth } from "./firebase";
-import api from "../apiService";
 import {
-  signInWithPopup,
   GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
+import api from "../apiService";
+import { auth } from "./firebase";
 
-export const doSignInWithGoogle = async (selectedCampus, loginWithCustomAuth) => {
-  if (!selectedCampus) {
-    throw new Error("selectedCampus is required");
+export const doSignInWithGoogle = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+
+    const user = result.user;
+    const it = await user.getIdToken();
+    const gGLoginViewModel={
+      idToken: it  
+    }
+    const response = await api.post("/Authentication/gg/login-with-google",gGLoginViewModel);
+    if (response.data) {
+      const token = response.data.Bear;
+      localStorage.setItem("token", token);
+      window.location.reload();
+    } else {
+      console.error("API không trả về token. Hủy đăng nhập.");
+      await doSignOut();
+    }
+  } catch (error) {
+    console.error("Lỗi khi đăng nhập với Google: ", error);
+    await doSignOut();
   }
-  const provider = new GoogleAuthProvider();
-  const result = await signInWithPopup(auth, provider);
-  const user = result.user;
-  // const response = await api.post("/Authentication/login-by-email", {
-  //   email: result.user.email,
-  //   campusId: selectedCampus
-  // });
-
-  // if (!response.data.bear || !response.data.campusId || !response.data.role) {
-  //   throw new Error(response.data.message);
-  // } 
-  // const token = response.data.bear;
-  // localStorage.setItem('token', token);
-  return user;
 };
 
 export const doSignOut = () => {
